b7a2b12649ceb0b8aa98e2076cf79d51a1f0b852
Jimbow <adeathjohn@gmail.com>
2014-07-20 08:56:28 +0800

    apk export

---
+++ Unity/Assets/_ObserverApp/OAModelMainVote.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OAModelMainVote : MonoBehaviour {
+	public int number;
+	public UILabel label;
+	public void add(){
+		number+=1;
+		label.text=number+"";
+	}
+}
